a0bc2e07ac5c653faaefc8da1482a54b80e58cc7
Nadim Radjab <nadimradjab94@gmail.com>
2021-09-04 14:33:57 +0200

    Config bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 module.exports = {
-  basePath: process.env.NEXT_NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: isProd ? process.env.NEXT_NEXT_PUBLIC_BASE_PATH : "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
 };
